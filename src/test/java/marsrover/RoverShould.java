63e3d6f1220033815cf54ec2ded41175092dd2ba
extern-dietert-christoph <extern.dietert_christoph@allianz.de>
2021-02-19 17:19:35 +0100

    refactoring added example test

--- src/test/java/marsrover/RoverShould.java
+++ src/test/java/marsrover/RoverShould.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RoverShould {
-
     Rover rover;
 
     @Test
-    void roverShould() {
+    void start_at_position_0_0_N() {
         rover = new Rover(new Grid());
-        assertEquals("0-0-N", rover.execute(""));
+        assertEquals("0:0:N", rover.execute(""));
+    }
+
+    @Test
+    void have_position_2_3_N_when_navigated_with_MMRMMLM() {
+        rover = new Rover(new Grid());
+        assertEquals("2:3:N", rover.execute("MMRMMLM"));
     }
 }
